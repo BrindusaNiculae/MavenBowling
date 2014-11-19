74d9294a37ff1df00f207aac92e4c7f499f4fe48
Brindu <Brindu@Brindu-PC>
2014-11-19 10:02:16 +0200

    BowlingRefactor1

---
+++ my-app/src/main/java/bowlingRefactor/BowlingJavaClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bowlingRefactor;
+
+/**
+ *
+ * @author Brindu
+ */
+public class BowlingJavaClass {
+    public void calculates(int [] rolls){}
+    
+}
---
+++ my-app/src/main/java/bowlingRefactor/Open.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bowlingRefactor;
+
+/**
+ *
+ * @author Brindu
+ */
+public class Open implements typeOfThrow{
+
+    @Override
+    public void wayToCalculate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
